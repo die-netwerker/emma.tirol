d42fb1797459867e92c2d23bc0ebd0f4b8c440de
Laura netwerk <l.ringlstetter@netwerk.at>
2025-03-24 10:13:48 +0100

    phpmyadmin installiert (lr)

---
+++ config/system/additional.php
@@ -0,0 +1,55 @@
+<?php
+
+if (getenv('IS_DDEV_PROJECT') == 'true') {
+    $GLOBALS['TYPO3_CONF_VARS'] = array_replace_recursive(
+        $GLOBALS['TYPO3_CONF_VARS'],
+        [
+            'DB' => [
+                'Connections' => [
+                    'Default' => [
+                        'dbname' => 'db',
+                        'driver' => 'mysqli',
+                        'host' => 'db',
+                        'password' => 'db',
+                        'port' => '3306',
+                        'user' => 'db',
+                    ],
+                ],
+            ],
+            // This GFX configuration allows processing by installed ImageMagick 6
+            'GFX' => [
+                'processor' => 'ImageMagick',
+                'processor_path' => '/usr/bin/',
+                'processor_path_lzw' => '/usr/bin/',
+            ],
+            // This mail configuration sends all emails to mailpit
+            'MAIL' => [
+                'transport' => 'smtp',
+                'transport_smtp_encrypt' => false,
+                'transport_smtp_server' => 'localhost:1025',
+            ],
+            'SYS' => [
+                'trustedHostsPattern' => '.*.*',
+                'devIPmask' => '*',
+                'displayErrors' => 1,
+            ],
+        ]
+    );
+} else {
+    $GLOBALS['TYPO3_CONF_VARS'] = array_replace_recursive(
+        $GLOBALS['TYPO3_CONF_VARS'],
+        [
+            'DB' => [
+                'Connections' => [
+                    'Default' => [
+                        'host' => $_ENV['TYPO3_DB_HOST'],
+                        'dbname' => $_ENV['TYPO3_DB_DBNAME'],
+                        'user' => $_ENV['TYPO3_DB_USER'],
+                        'password' => $_ENV['TYPO3_DB_PASSWORD'],
+                        'port' => $_ENV['TYPO3_DB_PORT'],
+                    ],
+                ],
+            ],
+        ],
+    );
+}
