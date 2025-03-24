d41a99ea25028a6ab9408736f58634d3f6ff22dc
Laura netwerk <l.ringlstetter@netwerk.at>
2025-03-24 11:51:36 +0100

    Modal erstellt (lr)

--- packages/nw_template/Resources/Public/JavaScript/all.js
+++ packages/nw_template/Resources/Public/JavaScript/all.js
@@ -12,6 +12,19 @@ function addScrolled() {
         document.body.classList.remove("scrolled");
     }
 };
+
+// modal
+document.querySelector("#top .menu").addEventListener("click", function() {
+    //document.getElementById("top").classList.toggle("sticky");
+    document.body.classList.toggle("modal");
+});
+document.addEventListener('DOMContentLoaded', function() {
+    var closeBtn = document.querySelector('.close-btn');
+
+    closeBtn.addEventListener('click', function(event) {
+        document.body.classList.remove("modal"); // Navigation schließen
+    });
+}); 
 window.addEventListener("scroll", addScrolled);
 
 
