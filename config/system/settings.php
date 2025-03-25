fe533ebd7c5714f0ac693b64628b4f12e70a3049
Laura netwerk <l.ringlstetter@netwerk.at>
2025-03-25 13:29:57 +0100

    Test

---
+++ config/system/settings.php
@@ -0,0 +1,161 @@
+<?php
+return [
+    'BE' => [
+        'debug' => true,
+        'passwordHashing' => [
+            'className' => 'TYPO3\\CMS\\Core\\Crypto\\PasswordHashing\\Argon2iPasswordHash',
+            'options' => [],
+        ],
+    ],
+    'DB' => [
+        'Connections' => [
+            'Default' => [
+                'charset' => 'utf8',
+                'driver' => 'mysqli',
+            ],
+        ],
+    ],
+    'EXTENSIONS' => [
+        'backend' => [
+            'backendFavicon' => '',
+            'backendLogo' => '',
+            'loginBackgroundImage' => '',
+            'loginFootnote' => '',
+            'loginHighlightColor' => '',
+            'loginLogo' => '',
+            'loginLogoAlt' => '',
+        ],
+        'cs_seo' => [
+            'cropDescription' => '0',
+            'evaluationDoktypes' => '1',
+            'evaluators' => 'Title,Description,H1,H2,HeadingOrder,Images,Keyword',
+            'excludeFileExtensions' => 'webp',
+            'forceMinDescription' => '1',
+            'inPageModule' => '0',
+            'maxDescription' => '156',
+            'maxH2' => '6',
+            'maxNavTitle' => '50',
+            'maxTitle' => '57',
+            'minDescription' => '140',
+            'minTitle' => '40',
+            'modFileColumns' => 'title,description',
+            'showDescriptionsInTCA' => '1',
+        ],
+        'extensionmanager' => [
+            'automaticInstallation' => '1',
+            'offlineMode' => '0',
+        ],
+        'ns_t3ai' => [
+            'apiKey' => '',
+            'model' => 'gpt-3.5-turbo',
+            'openAiPromptPrefixPageTitle' => 'Act as an SEO expert and write five an optimized title tag for a web page about [Content]',
+        ],
+        'staticfilecache' => [
+            'backendDisplayMode' => 'both',
+            'boostMode' => '0',
+            'cacheTagsEnable' => '0',
+            'clearCacheForAllDomains' => '1',
+            'cspGenerationOverride' => '0',
+            'debugHeaders' => '0',
+            'disableInDevelopment' => '0',
+            'enableGeneratorBrotli' => '0',
+            'enableGeneratorGzip' => '1',
+            'enableGeneratorPhp' => '0',
+            'enableGeneratorPlain' => '0',
+            'hashUriInCache' => '0',
+            'htaccessTemplateName' => 'EXT:staticfilecache/Resources/Private/Templates/Htaccess.html',
+            'inlineAssetsFileSize' => '50000',
+            'inlineScriptMinify' => '0',
+            'inlineServiceFavIcon' => '0',
+            'inlineServiceScripts' => '0',
+            'inlineServiceStyles' => '0',
+            'inlineStyleAssets' => 'ico,png,woff2',
+            'inlineStyleMinify' => '0',
+            'largeIdentifierInCacheTable' => '0',
+            'overrideCacheDirectory' => '',
+            'overrideClientUserAgent' => '',
+            'phpTemplateName' => 'EXT:staticfilecache/Resources/Private/Templates/Php.html',
+            'rawurldecodeCacheFileName' => '0',
+            'renameTablesToOtherPrefix' => '0',
+            'sendCacheControlHeaderRedirectAfterCacheTimeout' => '0',
+            'sendHttp2PushEnable' => '0',
+            'sendHttp2PushFileExtensions' => 'css,js',
+            'sendHttp2PushFileLimit' => '10',
+            'sendHttp2PushLimitToArea' => '',
+            'useFallbackMiddleware' => '1',
+            'useReverseUriLengthInPriority' => '1',
+            'validFallbackHeaders' => 'Content-Type,Content-Language,Content-Security-Policy,Link,X-SFC-Tags',
+            'validHtaccessHeaders' => 'Content-Type,Content-Language,Content-Security-Policy,Link,X-SFC-Tags',
+        ],
+    ],
+    'FE' => [
+        'cacheHash' => [
+            'enforceValidation' => true,
+        ],
+        'debug' => true,
+        'disableNoCacheParameter' => true,
+        'passwordHashing' => [
+            'className' => 'TYPO3\\CMS\\Core\\Crypto\\PasswordHashing\\Argon2iPasswordHash',
+            'options' => [],
+        ],
+    ],
+    'GFX' => [
+        'processor' => 'GraphicsMagick',
+        'processor_effects' => false,
+        'processor_enabled' => true,
+        'processor_path' => '/usr/bin/',
+    ],
+    'LOG' => [
+        'TYPO3' => [
+            'CMS' => [
+                'deprecations' => [
+                    'writerConfiguration' => [
+                        'notice' => [
+                            'TYPO3\CMS\Core\Log\Writer\FileWriter' => [
+                                'disabled' => false,
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ],
+    ],
+    'MAIL' => [
+        'transport' => 'sendmail',
+        'transport_sendmail_command' => '/usr/local/bin/mailpit sendmail -t --smtp-addr 127.0.0.1:1025',
+        'transport_smtp_encrypt' => '',
+        'transport_smtp_password' => '',
+        'transport_smtp_server' => '',
+        'transport_smtp_username' => '',
+    ],
+    'SYS' => [
+        'UTF8filesystem' => true,
+        'caching' => [
+            'cacheConfigurations' => [
+                'hash' => [
+                    'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+                ],
+                'pages' => [
+                    'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+                    'options' => [
+                        'compression' => true,
+                    ],
+                ],
+                'rootline' => [
+                    'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+                    'options' => [
+                        'compression' => true,
+                    ],
+                ],
+            ],
+        ],
+        'devIPmask' => '*',
+        'displayErrors' => 1,
+        'encryptionKey' => '16f087a003d0d0c9aa90adc1f0246b749dee01dc77fb1543ede842a0b92436a793917419d18803bba57b899e6128fa58',
+        'exceptionalErrors' => 12290,
+        'features' => [
+            'frontend.cache.autoTagging' => true,
+        ],
+        'sitename' => 'New TYPO3 site',
+    ],
+];
