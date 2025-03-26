b354691085420fe2b4ac2cbc928cd7b70513ccd8
Laura netwerk <l.ringlstetter@netwerk.at>
2025-03-26 14:29:53 +0100

    neues Element: Teaser Black (lr)

--- packages/nw_template/Resources/Public/JavaScript/all.js
+++ packages/nw_template/Resources/Public/JavaScript/all.js
@@ -27,5 +27,23 @@ document.addEventListener('DOMContentLoaded', function() {
 }); 
 window.addEventListener("scroll", addScrolled);
 
+//Animation Teaser Black
+document.addEventListener("DOMContentLoaded", function () {
+    const items = document.querySelectorAll(".frame-type-netwerk_teaserblack ul.items > li");
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("visible");
+                observer.unobserve(entry.target);
+            }
+        });
+    }, { threshold: 0.2 });
+
+    items.forEach(item => {
+        observer.observe(item);
+    });
+});
+
 
 
